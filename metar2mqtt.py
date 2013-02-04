3b8da6dd046a0da4dd04f695061a4fb62a2d5e25
Dennis Sell <github@sellfamily.us>
2013-02-04 02:20:09 -0600

    Reworked to function as a daemon

--- metar2mqtt.py
+++ metar2mqtt.py
@@ -4,7 +4,7 @@
 
 #
 # metar-bridge
-#	Provides airport weather data
+#    Provides airport weather data
 #
 
 
@@ -12,196 +12,92 @@ __author__ = "Dennis Sell"
 __copyright__ = "Copyright (C) Dennis Sell"
 
 
+APPNAME = "metar2mqtt"
+VERSION = "0.7"
+WATCHTOPIC = "/raw/" + APPNAME + "/command"
 
-import os
 import sys
+import os
 import mosquitto
 import socket
 import time
+import subprocess
 import logging
 import signal
 import threading
-from config import Config
 import pymetar
 import commands
+from daemon import Daemon
+from mqttcore import MQTTClientCore
+from mqttcore import main
 
 
-CLIENT_VERSION = "0.6"
-CLIENT_NAME = "metar2mqtt"
-MQTT_TIMEOUT = 60	#seconds
+class MyMQTTClientCore(MQTTClientCore):
+    def __init__(self, appname, clienttype):
+        MQTTClientCore.__init__(self, appname, clienttype)
+        self.clientversion = VERSION
+        self.watchtopic = WATCHTOPIC
+        self.metarids = self.cfg.METAR_IDS
+        self.interval = self.cfg.INTERVAL
+        self.clientversion = VERSION
+        self.t = threading.Timer(self.interval, self.do_thread_loop)
+        self.t.start()
+
+    def do_thread_loop(self):
+        while(self.running):
+            if ( self.mqtt_connected ):    
+                for station in self.metarids:
+                    try:
+                        rf=pymetar.ReportFetcher(station)
+                        rep=rf.FetchReport()
+                    except Exception, e:
+                        sys.stderr.write("Something went wrong when fetching the report.\n")
+                        sys.stderr.write("These usually are transient problems if the station ")
+                        sys.stderr.write("ID is valid. \nThe error encountered was:\n")
+                        sys.stderr.write(str(e)+"\n")
+                        sys.exit(1)
+                    rp=pymetar.ReportParser()
+                    pr=rp.ParseReport(rep)
+                    self.mqttc.publish(self.clientbase + station + "/time", str(pr.getISOTime()), qos=1, retain=True)
+                    self.mqttc.publish(self.clientbase + station + "/temperature", str(pr.getTemperatureFahrenheit()), qos=1, retain=True)
+                    if pr.getWindchillF() is not None:
+                        self.mqttc.publish(self.clientbase + station + "/windchill", str(pr.getWindchillF()), qos=1, retain=True)
+                    else:
+                        self.mqttc.publish(self.clientbase + station + "/windchill", "Unknown", qos=1, retain=True)    
+                    self.mqttc.publish(self.clientbase + station + "/humidity", str(pr.getHumidity()), qos=1, retain=True)
+                    if pr.getWindSpeed() is not None:
+                        self.mqttc.publish(self.clientbase + station + "/wind_speed", str(pr.getWindSpeed()), qos=1, retain=True)
+                    else:
+                        self.mqttc.publish(self.clientbase + station + "/wind_speed", "Unknown", qos=1, retain=True)
+                #    if pr.getWindDirection() is not None:
+                        self.mqttc.publish(self.clientbase + station + "/wind_direction", pr.getWindDirection(), qos=1, retain=True)
+                #    else:
+                #        self.mqttc.publish(self.clientbase + station + "/wind_direction", "Unknown", qos=1, retain=True)
+                    if pr.getWindCompass() is not None:
+                        self.mqttc.publish(self.clientbase + station + "/wind_compass", pr.getWindCompass(), qos=1, retain=True)
+                    else:
+                        self.mqttc.publish(self.clientbase + station + "/wind_compass", "Unknown", qos=1, retain=True)
+                    if pr.getPressure() is not None:
+                        self.mqttc.publish(self.clientbase + station + "/pressure", str(pr.getPressure()), qos=1, retain=True)
+                    else:
+                        self.mqttc.publish(self.clientbase + station + "/pressure", "Unknown", qos=1, retain=True)
+    #TODO need to add pressure trend!!!!!
+                    self.mqttc.publish(self.clientbase + station + "/dew_point", str(pr.getDewPointFahrenheit()), qos=1, retain=True)
+                    if pr.getCloudtype() is not None:
+                        self.mqttc.publish(self.clientbase + station + "/cloud_type", pr.getCloudtype(), qos=1, retain=True)
+                    else:
+                        self.mqttc.publish(self.clientbase + station + "/cloud_type", "Unknown", qos=1, retain=True)
+                    self.mqttc.publish(self.clientbase + station + "/sky_conditions", pr.getSkyConditions(), qos=1, retain=True)
+                if ( self.interval ):
+                    print "Waiting ", self.interval, " minutes for next update."
 
 
-#TODO might want to add a lock file
-#TODO  need to deal with no config file existing!!!
-#TODO move config file to home dir
+class MyDaemon(Daemon):
+    def run(self):
+        mqttcore = MyMQTTClientCore(APPNAME, clienttype="type1")
+        mqttcore.main_loop()
 
 
-#read in configuration file
-homedir = os.path.expanduser("~")
-f = file(homedir + '/.metar2mqtt.conf')
-cfg = Config(f)
-MQTT_HOST = cfg.MQTT_HOST
-MQTT_PORT = cfg.MQTT_PORT
-CLIENT_TOPIC = cfg.CLIENT_TOPIC
-BASE_TOPIC = cfg.BASE_TOPIC
-METAR_IDS = cfg.METAR_IDS
-INTERVAL = cfg.INTERVAL
-
-
-mqtt_connected = 0
-
-
-#define what happens after connection
-def on_connect(self, obj, rc):
-	global mqtt_connected
-	global running
-
-	mqtt_connected = True
-	print "MQTT Connected"
-	mqttc.publish( CLIENT_TOPIC + "status" , "online", 1, 1 )
-	mqttc.publish( CLIENT_TOPIC + "version", CLIENT_VERSION, 1, 1 )
-	ip = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
-	mqttc.publish( CLIENT_TOPIC + "ip", ip, 1, 1 )
-	mqttc.publish( CLIENT_TOPIC + "pid", os.getpid(), 1, 1 )
-	mqttc.subscribe( CLIENT_TOPIC + "ping", 2)
-
-
-def on_message(self, obj, msg):
-	if (( msg.topic == CLIENT_TOPIC + "ping" ) and ( msg.payload == "request" )):
-		mqttc.publish( CLIENT_TOPIC + "ping", "response", qos = 1, retain = 0 )
-
-
-def do_metar_loop():
-	global running
-	global METAR_IDS
-	global mqttc
-
-	while ( running ):
-		if ( mqtt_connected ):
-			for station in METAR_IDS:
-				try:
-					rf=pymetar.ReportFetcher(station)
-					rep=rf.FetchReport()
-				except Exception, e:
-					sys.stderr.write("Something went wrong when fetching the report.\n")
-					sys.stderr.write("These usually are transient problems if the station ")
-					sys.stderr.write("ID is valid. \nThe error encountered was:\n")
-					sys.stderr.write(str(e)+"\n")
-					sys.exit(1)
-
-				rp=pymetar.ReportParser()
-				pr=rp.ParseReport(rep)
-
-				mqttc.publish(BASE_TOPIC + station + "/time", str(pr.getISOTime()), 1, 1)
-				mqttc.publish(BASE_TOPIC + station + "/temperature", str(pr.getTemperatureFahrenheit()), 1, 1)
-				if pr.getWindchillF() is not None:
-					mqttc.publish(BASE_TOPIC + station + "/windchill", str(pr.getWindchillF()), 1, 1)
-				else:
-					mqttc.publish(BASE_TOPIC + station + "/windchill", "Unknown", 1, 1)	
-				mqttc.publish(BASE_TOPIC + station + "/humidity", str(pr.getHumidity()), 1, 1)
-				if pr.getWindSpeed() is not None:
-					mqttc.publish(BASE_TOPIC + station + "/wind_speed", str(pr.getWindSpeed()), 1, 1)
-				else:
-					mqttc.publish(BASE_TOPIC + station + "/wind_speed", "Unknown", 1, 1)
-			#	if pr.getWindDirection() is not None:
-					mqttc.publish(BASE_TOPIC + station + "/wind_direction", pr.getWindDirection(), 1, 1)
-			#	else:
-			#		mqttc.publish(BASE_TOPIC + station + "/wind_direction", "Unknown", 1, 1)
-				if pr.getWindCompass() is not None:
-					mqttc.publish(BASE_TOPIC + station + "/wind_compass", pr.getWindCompass(), 1, 1)
-				else:
-					mqttc.publish(BASE_TOPIC + station + "/wind_compass", "Unknown", 1, 1)
-				if pr.getPressure() is not None:
-					mqttc.publish(BASE_TOPIC + station + "/pressure", str(pr.getPressure()), 1, 1)
-				else:
-					mqttc.publish(BASE_TOPIC + station + "/pressure", "Unknown", 1, 1)
-				mqttc.publish(BASE_TOPIC + station + "/dew_point", str(pr.getDewPointFahrenheit()), 1, 1)
-				if pr.getCloudtype() is not None:
-					mqttc.publish(BASE_TOPIC + station + "/cloud_type", pr.getCloudtype(), 1, 1)
-				else:
-					mqttc.publish(BASE_TOPIC + station + "/cloud_type", "Unknown", 1, 1)
-				mqttc.publish(BASE_TOPIC + station + "/sky_conditions", pr.getSkyConditions(), 1, 1)
-			if ( INTERVAL ):
-				print "Waiting ", INTERVAL, " minutes for next update."
-				time.sleep(60 * INTERVAL)
-			else:
-				running = False	#do a single shot
-				print "Querries complete."
-		pass
-
-
-def do_disconnect():
-       global mqtt_connected
-       mqttc.disconnect()
-       mqtt_connected = False
-       print "Disconnected"
-
-
-def mqtt_disconnect():
-	global mqtt_connected
-	print "Disconnecting..."
-	mqttc.disconnect()
-	if ( mqtt_connected ):
-		mqtt_connected = False 
-		print "MQTT Disconnected"
-		mqttc.publish ( CLIENT_TOPIC + "/status" , "offline", 1, 1 )
-
-
-def mqtt_connect():
-	rc = 1
-	while ( rc ):
-		print "Attempting connection..."
-		mqttc.will_set( CLIENT_TOPIC + "/status", "disconnected", 1, 1)
-
-		#define the mqtt callbacks
-		mqttc.on_message = on_message
-		mqttc.on_connect = on_connect
-#		mqttc.on_disconnect = on_disconnect
-
-		#connect
-		rc = mqttc.connect( MQTT_HOST, MQTT_PORT, MQTT_TIMEOUT )
-		if rc != 0:
-			logging.info( "Connection failed with error code $s, Retrying in 30 seconds.", rc )
-			print "Connection failed with error code ", rc, ", Retrying in 30 seconds." 
-			time.sleep(30)
-		else:
-			print "Connect initiated OK"
-
-
-def cleanup(signum, frame):
-	mqtt_disconnect()
-	sys.exit(signum)
-
-
-#create a client
-mqttc = mosquitto.Mosquitto( CLIENT_NAME ) 
-
-#trap kill signals including control-c
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-running = True
-
-t = threading.Thread(target=do_metar_loop)
-t.start()
-
-
-def main_loop():
-	global mqtt_connected
-	mqttc.loop(10)
-	while running:
-		if ( mqtt_connected ):
-			rc = mqttc.loop(10)
-			if rc != 0:	
-				mqtt_disconnect()
-				print rc
-				print "Stalling for 20 seconds to allow broker connection to time out."
-				time.sleep(20)
-				mqtt_connect()
-				mqttc.loop(10)
-		pass
-
-
-mqtt_connect()
-main_loop()
-
+if __name__ == "__main__":
+    daemon = MyDaemon('/tmp/' + APPNAME + '.pid')
+    main(daemon)
